dec78d11d79f19f3b65782dabc013d9282f6557c
Felipe Farias <fefarias97@gmail.com>
2021-08-22 20:45:00 -0300

    Resolução do exercício

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace olamundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string meunome = "Felipe Francisco";
+            Console.WriteLine("Eu me chamo "+meunome);
+        }
+    }
+}
